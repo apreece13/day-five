4d6cf89e6e9b1cbf4d43584ef214318c1e98c5b5
Austin Preece <austinpreece13@gmail.com>
2020-03-24 15:46:08 -0400

    day-five

---
+++ script.js
@@ -0,0 +1,56 @@
+// In your JavaScript file, declare a new variable called likesToTravel and set it equal to true or false.
+// If likesToTravel is true, log "Bon voyage!" to the console. If not, log "Enjoy your couch!" to the console.
+// :hahaa:
+// 5
+
+// var likesToTravel = true;
+
+// console.log(likesToTravel);
+
+// if(likesToTravel === true){
+//     console.log("Bon voyage!")
+// } else {"Good, because we are quarantined anyway"
+//     }
+// if(2 + 2 === 4){
+//     console.log
+// // }
+
+
+// var favoriteFood = "fried pickles";
+
+// if( favoriteFood === "fried pickles" || (favoriteFood === "pepperoni")
+
+// console.log("Congradulations! You have excellent taste!");
+
+
+// Use JavaScript to check and see if the following comparisons are true. For each of the following, log "True fact!" to the console if the condition evaluates to true and "LIES!" to the console if it doesn't.
+// Is the sum of 1 and 1 greater than 5?
+// Is the product of 4 and 5 less than our equal to 20?
+// Is the difference between 6 and 2 greater than or equal to 0?
+
+// While Loop
+
+// var birdCounter = 1;
+
+// while(birdCounter <= 100) {
+//     console.log(birdCounter, "TWEET TWEET")
+//     BirdCounter
+
+// }
+
+// if(counter <= 100){
+//     console.log("TWEET TWEET")
+// }
+
+var interests = ["yoga", "soccer", "cooking", "music", "skiing"]
+
+for(var i = 0; i < 5; i ++){
+    console.log(interests[i], "is a thing I enjoy")
+}
+
+if(interests [i]===yoga){
+    console.log ("i enjoy", interests[i])
+}
+else {
+    console.log ("i enjoy,", interests[1])
+}
